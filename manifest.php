6e254a10c5fc72d422049293b31fe5436e431b16
Leon Nikitin <nlv@lab321.ru>
2016-08-20 18:19:20 +0600

    Добавлены модули проекты и проектные задачи

--- manifest.php
+++ manifest.php
@@ -6,9 +6,9 @@ $manifest = array(
     'author' => 'Leon.V.Nikitin@pravmail.ru',
     'description' => 'Добавление направлений в основные модули (semlot)',
     'is_uninstallable' => true,
-    'published_date' => '2016-08-18',
+    'published_date' => '2016-08-19',
     'type' => 'module',
-    'version' => '1.0.0',
+    'version' => '1.1.0',
 );
 $installdefs = array(
     'id' => 'semlot_sectors',
@@ -18,12 +18,30 @@ $installdefs = array(
         'to_module' => 'Tasks',
         'language' => 'ru_ru',
       ),
+      array (
+        'from' => '<basepath>/source/modules/Project/language/ru_ru.lang.php',
+        'to_module' => 'Project',
+        'language' => 'ru_ru',
+      ),
+      array (
+        'from' => '<basepath>/source/modules/ProjectTask/language/ru_ru.lang.php',
+        'to_module' => 'ProjectTask',
+        'language' => 'ru_ru',
+      ),
 	),
 
   'vardefs' => array (
       array (
         'from' => '<basepath>/source/modules/Tasks/vardefs/vardefs.php',
           'to_module' => 'Tasks',
+      ),
+      array (
+        'from' => '<basepath>/source/modules/Project/vardefs/vardefs.php',
+          'to_module' => 'Project',
+      ),
+      array (
+        'from' => '<basepath>/source/modules/ProjectTask/vardefs/vardefs.php',
+          'to_module' => 'ProjectTask',
       ),
   ),
 );
